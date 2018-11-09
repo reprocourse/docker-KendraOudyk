351b87a9396ee41269bd2dc178797f3ba2ed043f
KendraOudyk <kendra.oudyk@gmail.com>
2018-11-09 10:00:53 -0500

    parsing data path into docker run command

--- hie_analysis.py
+++ hie_analysis.py
@@ -2,9 +2,16 @@
 import pandas as pd
 import numpy as np
 from statsmodels.formula.api import MNLogit
+import argparse
 
 # LOAD DATA
-hie = pd.read_csv('./data/hie_data.csv', na_values='nd')
+## allow user to specify the path
+parser = argparse.ArgumentParser(description='Process a csv file.')
+parser.add_argument('file')
+args = parser.parse_args()
+
+hie = pd.read_csv(args.file, na_values='nd')
+#hie = pd.read_csv('./data/hie_data.csv', na_values='nd')
 
 # DEFINE VARIABLES
 ## predictors (copeptin, NSE, and intercept)
