140861b2773bd47855c8157bdd61639e9e85c467
KendraOudyk <kendra.oudyk@gmail.com>
2018-11-15 17:11:41 -0500

    making the na_values in the data an argument

--- hie_analysis.py
+++ hie_analysis.py
@@ -4,22 +4,23 @@ import numpy as np
 from statsmodels.formula.api import MNLogit
 import argparse
 
-# LOAD DATA
-## allow user to specify the path
+# SET UP ARGPARSER to allow user to specify the path
 parser = argparse.ArgumentParser(description='Process a csv file.')
-parser.add_argument('file')
+parser.add_argument('file',help='name of the csv file containing the data')
+parser.add_argument('--not_a_number','-n',help='value for absent numbers in the data')
+
 args = parser.parse_args()
 
-hie = pd.read_csv(args.file, na_values='nd')
-#hie = pd.read_csv('./data/hie_data.csv', na_values='nd')
+# LOAD DATA
+hie = pd.read_csv(args.file, na_values=args.not_a_number)
 
 # DEFINE VARIABLES
-## predictors (copeptin, NSE, and intercept)
+#  predictors (copeptin, NSE, and intercept)
 x = hie[['copeptin_6h_pmol_l','NSE_6h_ng_ml']]
 x['intercept'] = 1.0
 
-## outcome
-y = hie['outcome']
+# outcome (i.e., death, etc)
+y = hie['outcome'] 
 
 # ANALYSIS
 model = MNLogit(y,x,missing='drop')
